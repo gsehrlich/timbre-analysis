e822116c9afc3a33773922fe3661150c0364841a
Gabriel Ehrlich <gabriel.s.ehrlich@gmail.com>
2016-04-05 16:51:02 -0700

    Got real-time kinda working

--- autofft.py
+++ autofft.py
@@ -5,10 +5,12 @@ plots its FFT afterwards."""
 from __future__ import division
 import sys, os
 import pyaudio, wave
+import numpy as np
 import time
-from show_transform import show_transform
+from PyQt4 import QtCore as core, QtGui as gui
+from show_transform import setup_plot, show_transform, show_transform_file, pg
 
-CHUNK = 1024
+CHUNK = 4096
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
@@ -17,8 +19,7 @@ DEFAULT_COUNTDOWN_SECONDS = 0
 
 p = pyaudio.PyAudio()
 
-def listen(record_seconds, countdown_seconds, filename):
-    recording = True
+def listen_file(record_seconds, countdown_seconds, filename):
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
                     rate=RATE,
@@ -65,7 +66,62 @@ def accept_time_input(ans, default=None):
             print "Input not understood."
             sys.exit()
 
-if __name__ == '__main__':
+class Listener(core.QObject):
+    new_chunk = core.pyqtSignal(bytearray)
+
+    def __init__(self):
+        super(Listener, self).__init__()
+
+        self.stream = p.open(format=FORMAT,
+                        channels=CHANNELS,
+                        rate=RATE,
+                        input=True,
+                        frames_per_buffer=CHUNK)
+
+        self.thread = core.QThread()
+        self.thread.setObjectName("listener thread")
+
+        print "creating singleShot"
+        core.QTimer.singleShot(0, self.thread.start)
+
+        self.moveToThread(self.thread)
+        self.thread.started.connect(self.run)
+
+    def run(self):
+        self.cont = True
+
+        while self.cont:
+            self.new_chunk.emit(bytearray(self.stream.read(CHUNK)))
+            #time.sleep(1)
+
+        self.stream.close()
+
+    def stop(self):
+        # Call this from a different thread
+        self.cont = False
+
+def run_realtime():
+    if FORMAT != pyaudio.paInt16:
+        raise Exception("This version hacked together; need FORMAT=paInt16")
+
+    app = gui.QApplication([])
+
+    def name_main_thread():
+        core.QThread.currentThread().setObjectName("main")
+    core.QTimer.singleShot(0, name_main_thread)
+
+    plot, curve = setup_plot()
+    def plot_here(bytearray_stereo):
+        chunk_arr = np.frombuffer(bytearray_stereo, dtype=np.int16)
+        chunk_arr = chunk_arr.reshape((len(chunk_arr)/2, 2))
+        show_transform(RATE, chunk_arr, plot=plot, curve=curve)
+
+    listener = Listener()
+    listener.new_chunk.connect(plot_here)
+
+    app.exec_()
+
+def run_record():
     ans = raw_input("Time to record? [%s s]: " % DEFAULT_RECORD_SECONDS)
     record_seconds = accept_time_input(ans, default=DEFAULT_RECORD_SECONDS)
 
@@ -84,5 +140,15 @@ if __name__ == '__main__':
             print "Quitting. Please try again."
             sys.exit()
 
-    filename = listen(record_seconds, countdown_seconds, filename)
-    show_transform(filename)
+    filename = listen_file(record_seconds, countdown_seconds, filename)
+    show_transform_file(filename)
+
+if __name__ == '__main__':
+    ans = raw_input("Enter A for real-time, B for record: ")
+    if ans.lower() == "a":
+        run_realtime()
+    elif ans.lower() == "b":
+        run_record()
+    else:
+        print "Input not understood. Quitting. Please try again."
+        sys.exit()
--- show_transform.py
+++ show_transform.py
@@ -4,26 +4,40 @@ from scipy.signal import periodogram
 from scipy.io import wavfile
 import pyqtgraph as pg
 
-def show_transform(filename):
-    rate, data_stereo = wavfile.read(filename)
-    x, y = periodogram(np.sum(data_stereo, axis=1), rate) # y is the PSD
-    p = pg.plot(x, y)
+def setup_plot():
+    p = pg.plot()
+    curve = p.plotItem.plot()
+
     g = p.plotItem.graphicsItem()
     g.enableAutoRange(enable=False)
     p.plotItem.setLogMode(x=False, y=True)
-    p.plotItem.setTitle("Power spectral density of <b>%s</b>" % filename)
+    p.plotItem.setTitle("Power spectral density")
     g.setMouseEnabled(y=False)
 
-    # Set good-looking Y range (and workable X range)
+    return p, curve
+
+def show_transform(rate, data_stereo, plot=None, curve=None):
+    x, y = periodogram(np.sum(data_stereo, axis=1), rate) # y is the PSD
+    if plot is None: plot = pg.plot(x[y != 0], y[y != 0])
+    else: curve.setData(x[y != 0], y[y != 0])
+
+    """
+    # Set good-looking Y range
     appx_noise_avg = np.log10(sum(y[len(y)//2:])/(len(y)/2))
     high = int(np.log10(max(y)))
     low = int(high - 1.11*(high - appx_noise_avg)) - 1
-    p.setYRange(low, high)
-    p.setYRange(low, high) # If I don't do it a second time, sometimes #1 fails
-    p.setXRange(0, len(y)/2)
+    plot.setYRange(low, high)
+    plot.setYRange(low, high) # If I don't do it a second time, sometimes #1 fails
+    """
+
+def show_transform_file(filename):
+    rate, data_stereo = wavfile.read(filename)
+    p, curve = setup_plot()
+    show_transform(rate, data_stereo, curve)
+    p.plotItem.setTitle("Power spectral density of <b>%s</b>" % filename)
 
     # Pause to show graph
     raw_input("Press enter to quit")
 
 if __name__ == "__main__":
-    show_transform(sys.argv[1])
+    show_transform_file(sys.argv[1])
